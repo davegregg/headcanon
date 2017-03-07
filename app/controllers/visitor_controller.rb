a365f2df8dcd514fddd31538d67e456a654a45bd
davegregg <davegregg@gmail.com>
2017-03-06 22:28:48 -0500

    Blog-like page.

---
+++ app/controllers/visitor_controller.rb
@@ -0,0 +1,7 @@
+class VisitorController < ApplicationController
+
+  def index
+    @page_title = 'Everything is Headcanon.'
+  end
+
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_TIY_6_1_welcome_to_the_frontend_session'
