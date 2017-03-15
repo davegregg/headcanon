16533e52fc26684110330d16b3640277bed50584
davegregg <davegregg@gmail.com>
2017-03-14 22:09:01 -0400

    Finished user editing; Added user creation; Started user showing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
 
     def index
       @users = User.all
+      render json: @users
+    end
+
+    def show
+      @user = User.find(params[:id])
     end
 
     def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  ## ignore any periods not followed by `json` and the EOS
+  # permit_periods_in = lambda do |*vars|
+  #   match = /[^\/]+(?=\.json\z)|[^\/]+/
+  #   contraints = vars.map{ |var| [var, match] }.to_h
+  #   return {constraints: contraints}
+  # end
+  ## use in place of the 'constraints' option, e.g.:
+  ## resources :users, **permit_periods_in.(:email)
+
   resources :users
   resources :posts
 
